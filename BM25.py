08634f161da08b4792fcedf598f149aad582cf67
Daniel Cohen Larsen <daniel.clarsen@gmail.com>
2019-04-11 12:18:41 +0200

    :)

--- BM25.py
+++ BM25.py
@@ -1,7 +1,5 @@
-from gensim.summarization.bm25 import *
-
+import gensim
 corpus = ["black", "cat", "white", "cat"], ["cat", "outer", "space"], ["wag", "dog"]
 
 gensim.summarization.bm25.BM25(corpus)
 
-gens
